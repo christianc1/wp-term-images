4ffd8ff6dfc94aa6a1a2cf8ca052155138e7c1f5
Christian Chung <christian.m.chung@gmail.com>
2016-08-16 21:19:20 -0400

    Adjust some javascript to allow for multi instance editting on edit screen

--- assets/js/term-image.js
+++ assets/js/term-image.js
@@ -1,7 +1,7 @@
 /* global i10n_WPTermImages, ajaxurl */
 jQuery( document ).ready( function( $ ) {
     'use strict';
-
+	
 	/* Globals */
 	var wp_term_images_modal;
 
@@ -78,27 +78,30 @@ jQuery( document ).ready( function( $ ) {
 				library:  { type: 'image' },
 				multiple: false
 			} );
-
-			// Picking an image
-			wp_term_images_modal.on( 'select', function () {
-
-				// Get the image URL
-				var image = wp_term_images_modal.state().get( 'selection' ).first().toJSON();
-
-				if ( '' !== image ) {
-					if ( ! $( element ).hasClass( 'quick' ) ) {
-						$( '#term-image' ).val( image.id );
-						$( '#wp-term-images-photo' ).attr( 'src', image.url ).show();
-						$( '.wp-term-images-remove' ).show();
-					} else {
-						$( 'button.wp-term-images-media' ).hide();
-						$( 'a.button', '.inline-edit-row' ).show();
-						$( ':input[name="term-image"]', '.inline-edit-row' ).val( image.id );
-						$( 'img.wp-term-images-media', '.inline-edit-row' ).attr( 'src', image.url ).show();
-					}
-				}
-			} );
 		}
+		// Picking an image, set this Callback to EXECUTE ONCE
+		wp_term_images_modal.once( 'select', function () {
+
+			// Get the image URL
+			var image = wp_term_images_modal.state().get( 'selection' ).first().toJSON();
+
+			// Get the container
+			var $container = $(element).parent();
+			console.log($container);
+
+			if ( '' !== image ) {
+				if ( ! $( element ).hasClass( 'quick' ) ) {
+					$container.find('input').show().val( image.id );
+					$container.find( '#wp-term-images-photo' ).attr( 'src', image.url ).show();
+					$container.find( '.wp-term-images-remove' ).show();
+				} else {
+					$( 'button.wp-term-images-media' ).hide();
+					$( 'a.button', '.inline-edit-row' ).show();
+					$( ':input[name="term-image"]', '.inline-edit-row' ).val( image.id );
+					$( 'img.wp-term-images-media', '.inline-edit-row' ).attr( 'src', image.url ).show();
+				}
+			}
+		} );
 
 		// Open the modal
 		wp_term_images_modal.open();
@@ -113,12 +116,15 @@ jQuery( document ).ready( function( $ ) {
 	 */
 	function wp_term_images_reset( element, event ) {
 		event.preventDefault();
+		
+		// Get the container
+		var $container = $(element).parent();
 
 		// Clear image metadata
 		if ( ! $( element ).hasClass( 'quick' ) ) {
-			$( '#term-image' ).val( 0 );
-			$( '#wp-term-images-photo' ).attr( 'src', '' ).hide();
-			$( '.wp-term-images-remove' ).hide();
+			$container.find( 'input' ).val( 0 );
+			$container.find( '#wp-term-images-photo' ).attr( 'src', '' ).hide();
+			$container.find( '.wp-term-images-remove' ).hide();
 		} else {
 			$( ':input[name="term-image"]', '.inline-edit-row' ).val( '' );
 			$( 'img.wp-term-images-media', '.inline-edit-row' ).attr( 'src', '' ).hide();
